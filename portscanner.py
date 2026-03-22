820d1ff40725891741e777d1075a2bf1214dbc82
maikanmaciji <maikanmaciji@gmail.com>
2026-03-22 05:39:44 +0400

    Refactor port scanning logic for improved readability and error handling

--- portscanner.py
+++ portscanner.py
@@ -6,20 +6,19 @@ def scan(target, ports):
 
 def scan_port(ipaddress, port):
 	try:
-		sock = socket.socket()
-		socket.connect((ipaddress, port))
-		print("[+] Port Opende" + str(port))
+			sock = socket.socket()
+			socket.connect((ipaddress, port))
+			print('[+] Port Opende ' + str(port))
+			socket.close()
 	except:
-		print("[-] Port Closed" + str(port))
+			print('[-] Port Closed ' + str(port))
 
 
-targets = input("[*] Enter targets to scan (split them by ,) : ")
-ports = input("[*] How many ports do you want to scan?: ")
-if "," in targets:
-	print("[*] scanning multiple targets")
-	for ip_addr in targets.split(','):
-		scan(ip_addr.split(' '),ports)
+targets = input('[*] Enter targets to scan (split them by ,) : ')
+ports = int(input('[*] How many ports do you want to scan?: '))
+if ',' in targets:
+		print('[*] scanning multiple targets')
+		for ip_addr in targets.split(','):
+				scan(ip_addr.strip(' '),ports)
 else:
-	can(targets, port);
-scan(targets,ports)
-
+			scan(targets, ports)
